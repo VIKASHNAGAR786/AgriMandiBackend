de38bc212d11e579039ed7a3e1aed1bad5f50f0e
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-01-15 12:36:01 +0530

    update endpoints

--- src/main/java/com/example/AgriMandi/config/WebConfig.java
+++ src/main/java/com/example/AgriMandi/config/WebConfig.java
@@ -2,18 +2,21 @@ package com.example.AgriMandi.config;
 
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${allowed.origin}")
+	private String allowedOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-//                .allowedOrigins("https://agrimandibackend-production.up.railway.app")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
